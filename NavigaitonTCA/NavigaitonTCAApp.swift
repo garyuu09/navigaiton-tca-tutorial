66c9c9c2e08c38bab30040e9eace07820d2f2c96
garyuu09 <ribereo.minami@gmail.com>
2024-02-24 20:41:09 +0900

    Initial Commit

---
+++ NavigaitonTCA/NavigaitonTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigaitonTCAApp.swift
+//  NavigaitonTCA
+//
+//  Created by Ryuga on 2024/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigaitonTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
